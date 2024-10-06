f1e1576a38662934daabedf7697e2071b37b4cb1
Erwin Darsono <erwindarsono08@gmail.com>
2024-10-06 21:47:40 +0800

    send file to client OK

--- src/main/java/org/example/EchoEndpoint.java
+++ src/main/java/org/example/EchoEndpoint.java
@@ -5,10 +5,14 @@ import jakarta.websocket.OnOpen;
 import jakarta.websocket.Session;
 import jakarta.websocket.server.ServerEndpoint;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.file.Files;
 
 @ServerEndpoint(value = "/echo")
 public class EchoEndpoint {
+    Session session;
     @OnOpen
     public void onOpen(Session session) throws IOException {
         Thread clientHandler = new Thread(new ClientHandler(session));
@@ -16,7 +20,27 @@ public class EchoEndpoint {
 //        session.getBasicRemote().sendText("You are connected to server!");
     }
     @OnMessage
-    public void onMessage(String message){
+    public void onMessage(String message, Session session){
         System.out.println("From client : " + message);
+        if(message.equalsIgnoreCase("yes")){
+            System.out.println("in!");
+            File file = new File("test.txt");
+            if(file.exists()){
+                try{
+                    byte[] fileBytes = Files.readAllBytes(file.toPath());
+
+                    ByteBuffer bytebuffer = ByteBuffer.wrap(fileBytes);
+
+                    session.getBasicRemote().sendBinary(bytebuffer);
+                    System.out.println("SENT!");
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+
+            }
+            else{
+                System.out.println("tak ada");
+            }
+        }
     }
 }
