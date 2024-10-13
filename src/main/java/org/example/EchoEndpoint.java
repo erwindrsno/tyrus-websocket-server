400f5baa8dac2c7db34a1f564bac67fb9d3e54a8
Erwin Darsono <erwindarsono08@gmail.com>
2024-10-13 23:54:02 +0700

    progress 2

--- src/main/java/org/example/EchoEndpoint.java
+++ src/main/java/org/example/EchoEndpoint.java
@@ -1,8 +1,8 @@
 package org.example;
 
+import jakarta.websocket.OnClose;
 import jakarta.websocket.OnMessage;
 import jakarta.websocket.OnOpen;
-import jakarta.websocket.OnClose;
 import jakarta.websocket.Session;
 import jakarta.websocket.server.ServerEndpoint;
 
@@ -16,14 +16,26 @@ public class EchoEndpoint {
     Session session;
     @OnOpen
     public void onOpen(Session session) throws IOException {
-        System.out.println("OnOpen");
-        session.getBasicRemote().sendText("Yay connected!");
+        System.out.println("Threads count currently  : " + Thread.currentThread().getName());
+        File file = new File("test.txt");
+        if(file.exists()){
+            try{
+                byte[] fileBytes = Files.readAllBytes(file.toPath());
+                ByteBuffer bytebuffer = ByteBuffer.wrap(fileBytes);
+                session.getBasicRemote().sendBinary(bytebuffer);
+                System.out.println("SENT!");
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+        else{
+            System.out.println("tak ada");
+        }
     }
 
     @OnMessage
     public void onMessage(String message, Session session) throws IOException{
         System.out.println("From client : " + message);
-        session.getAsyncRemote().sendText("You are connected!");
     }
 
     @OnClose
